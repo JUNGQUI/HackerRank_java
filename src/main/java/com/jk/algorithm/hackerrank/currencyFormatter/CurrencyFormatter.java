a46ac3e2defafad8d5f7f3f17c051319d24dfb79
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2019-09-11 14:55:44 +0900

    hacker rank second step
    CurrencyFormatter, EOF, StringIntroduce 문제 해결

---
+++ src/main/java/com/jk/algorithm/hackerrank/currencyFormatter/CurrencyFormatter.java
@@ -0,0 +1,22 @@
+package com.jk.algorithm.hackerrank.currencyFormatter;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class CurrencyFormatter {
+
+    public void solution () {
+        double payment = 123142.12;
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.US).format(Locale.CHINA);
+        String france = NumberFormat.getCurrencyInstance(Locale.US).format(Locale.FRANCE);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+
+}
---
+++ src/main/java/com/jk/algorithm/hackerrank/currencyFormatter/package-info.java
@@ -0,0 +1,5 @@
+package com.jk.algorithm.hackerrank.currencyFormatter;
+
+/**
+ * java NumberFormat 을 이용해서 환전하는 class를 만들어라. 단, India 의 경우 지원이 되지 않기 때문에 새롭게 locale을 생성히여 환전하라.
+ **/
