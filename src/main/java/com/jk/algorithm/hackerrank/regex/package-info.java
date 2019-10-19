7b1dbc64820ace6f3c8fc350d7f93259af4a6a0b
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2019-10-19 11:07:09 +0900

    hacker rank fifth step
    regex pattern ID 특정짓기

--- src/main/java/com/jk/algorithm/hackerrank/regex/package-info.java
+++ src/main/java/com/jk/algorithm/hackerrank/regex/package-info.java
@@ -1,5 +1,18 @@
 package com.jk.algorithm.hackerrank.regex;
 
-/**
- * regex pattern 을 만드는 문제, 주어진 string 이 IP Address 인지 확인
- **/
+/*
+  Regex : regex pattern 을 만드는 문제, 주어진 string 이 IP Address 인지 확인
+  ValidUserNameRegularExpression : 8자 이상의 글자만 입력받는 regular 정
+
+  자주 쓰이는 패턴
+  1) 숫자만 : ^[0-9]*$
+  2) 영문자만 : ^[a-zA-Z]*$
+  3) 한글만 : ^[가-힣]*$
+  4) 영어 & 숫자만 : ^[a-zA-Z0-9]*$
+  5) E-Mail : ^[a-zA-Z0-9]+@[a-zA-Z0-9]+$
+  6) 휴대폰 : ^01(?:0|1|[6-9]) - (?:\d{3}|\d{4}) - \d{4}$
+  7) 일반전화 : ^\d{2,3} - \d{3,4} - \d{4}$
+  8) 주민등록번호 : \d{6} \- [1-4]\d{6}
+  9) IP 주소 : ([0-9]{1,3}) \. ([0-9]{1,3}) \. ([0-9]{1,3}) \. ([0-9]{1,3})
+  출처: https://highcode.tistory.com/6 [HighCode]
+ */
