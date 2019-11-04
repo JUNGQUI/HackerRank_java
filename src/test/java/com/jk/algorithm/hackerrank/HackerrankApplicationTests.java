64c3994d24a7e563d2131da4996ca3c8b60aac47
JUNGQUI <39726558+JUNGQUI@users.noreply.github.com>
2019-11-04 14:32:58 +0900

    hacker rank eighth step
    Primality problem solve.

--- src/test/java/com/jk/algorithm/hackerrank/HackerrankApplicationTests.java
+++ src/test/java/com/jk/algorithm/hackerrank/HackerrankApplicationTests.java
@@ -5,9 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.math.BigInteger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,25 +15,10 @@ public class HackerrankApplicationTests {
 
     @Test
     public void contextLoads() {
-        int n = 9;
-        String[] s = {"-100", "50", "0", "56.6", "90", "0.12", ".12", "02.34", "000.000"};
+        BigInteger a = new BigInteger(String.valueOf(13));
+        boolean factor = a.isProbablePrime(1);
 
-        Comparator<String> customComparator = new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                BigDecimal a = new BigDecimal(s1);
-                BigDecimal b = new BigDecimal(s2);
-                return b.compareTo(a); // descending order
-            }
-        };
-
-        Arrays.sort(s, 0, n, customComparator);
-
-        //Output
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(s[i]);
-        }
+        System.out.println("J tag");
     }
 
 }
