57f3afac7c44a6f438d65d485cab23cbd008b074
JUNGQUI <ljk2518@gmail.com>
2020-01-20 18:04:17 +0900

    hacker rank Sock Merchant

--- src/test/java/com/jk/algorithm/hackerrank/HackerrankApplicationTests.java
+++ src/test/java/com/jk/algorithm/hackerrank/HackerrankApplicationTests.java
@@ -4,9 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.StringUtils;
 
-import java.util.*;
+import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,59 +13,34 @@ public class HackerrankApplicationTests {
 
 //    Pattern pattern = Pattern.compile("\\d{1,2}");
 
-    static class Student{
-        private int id;
-        private String fname;
-        private double cgpa;
-        public Student(int id, String fname, double cgpa) {
-            super();
-            this.id = id;
-            this.fname = fname;
-            this.cgpa = cgpa;
-        }
-        public int getId() {
-            return id;
-        }
-        public String getFname() {
-            return fname;
-        }
-        public double getCgpa() {
-            return cgpa;
-        }
-    }
+    @Test
+    public void contextLoads() {
+        int pair = 0;
 
-    static class CustomComparator implements Comparator<Student> {
+        int[] ar = new int[7];
+        ar[0] = 10;
+        ar[1] = 10;
+        ar[2] = 10;
+        ar[3] = 10;
+        ar[4] = 20;
+        ar[5] = 20;
+        ar[6] = 50;
 
-        @Override
-        public int compare(Student o1, Student o2) {
-            if (o1.getCgpa() == o2.getCgpa()) {
-                if (o1.getFname().compareTo(o2.getFname()) == 0) {
-                    return o1.getId() - o2.getId();
-                } else {
-                    return o1.getFname().compareTo(o2.getFname());
-                }
-            } else {
-                if (o1.getCgpa() > o2.getCgpa()) {
-                    return -1;
-                } else {
-                    return 1;
+        int[] tempArray = Arrays.copyOf(ar, ar.length);
+        int[] uniques = Arrays.stream(ar).distinct().toArray();
+
+
+        for (int unique : uniques) {
+            int sameValue = 0;
+
+            for (int i = 0; i < tempArray.length; i++) {
+                if (tempArray[i] != 0) {
+                    if (unique == tempArray[i]) {
+                        sameValue++;
+                        tempArray[i] = 0;
+                    }
                 }
             }
         }
-    }
-
-    @Test
-    public void contextLoads() {
-        ArrayList<Student> studentList = new ArrayList<>();
-
-        studentList.add(new Student(33, "Rumpa", 3.68));
-        studentList.add(new Student(85, "Ashis", 3.85));
-        studentList.add(new Student(56, "Samiha", 3.75));
-        studentList.add(new Student(19, "Samara", 3.75));
-        studentList.add(new Student(22, "Fahim", 3.76));
-
-        studentList.sort(new CustomComparator());
-
-        System.out.println("J tag");
     }
 }
